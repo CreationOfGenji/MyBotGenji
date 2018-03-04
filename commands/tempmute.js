75acfd8de4d39be7deaf3bc98f25aae311dff757
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-04 14:59:17 +0000

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
 
     let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!tomute) return message.reply("Couldn't Find User.");
-    if(tomute.hasPermission("ADMINISTRATOR")) return message.reply("Tou don't have permissions to use this command!");
+    if(tomute.hasPermission("ADMINISTRATOR")) return message.reply("Can't Mute Them! Sorry!");
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You don't have permission to use this command!");
     let muterole = message.guild.roles.find("name", "Genji-Mute");
     //start of create role
