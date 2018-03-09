38f648dd875c21d9f38582977a9bc85fa683ffa0
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-09 19:50:09 +0000

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
   if(!args[0]) return message.channel.send("no");
-  message.channel.bulkDelete(args[0]).then(() => {
+  message.channel.bulkDelete(args[0] + 1).then(() => {
   message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
 });
 
