d68491a82d7683c5e8318a6144bf03bd3aa95389
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-10 18:17:18 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,11 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-
+  
+      if(args[0] == "help"){
+        message.reply("Usage: G.mute <user> <time>");
+        return;
+      }
   PingEmbed = new Discord.RichEmbed()
   .setTitle("Pong!")
   .setColor("#c3bacc")
