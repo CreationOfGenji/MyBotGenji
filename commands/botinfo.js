c99acc7cd13df84c3ab651fc5bdc506871426df3
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-10 15:03:48 +0000

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
     .setThumbnail(bicon)
     .addField("Bot Name", bot.user.username)
     .addField("Created On", bot.user.createdAt)
-    .addField(name = "Created By", value "undefined#3779");
+    .addField(name = "Created By", value = "undefined#3779");
 
     return message.channel.send(botembed);
 
