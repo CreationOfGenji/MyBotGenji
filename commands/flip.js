e24f1eff6f05fe1adbf47b1a2b90ebe36fb50f61
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-10 14:47:27 +0000

    Add files via upload

---
+++ commands/flip.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+let replies = ["Heads", "Tails",];
+
+let result = Math.floor((Math.random()* replies.length));
+let question = args.slice(0).join(" ");
+
+let flipembed = new Discord.RichEmbed()
+.setColor("#1b0ece")
+.setAuthor(message.author.tag)
+.addField("The coin landed on", replies[result])
+
+message.channel.send(flipembed);
+
+
+}
+
+    module.exports.help = {
+        name: "flip"
+      }
