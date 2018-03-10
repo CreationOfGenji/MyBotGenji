a25ab8849ec29a8d9b5bebf5ff5ccff468b68085
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-10 11:08:52 +0000

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js);
+
+module.exports.run = async (bot, message, args) => {
+
+message.channel.send('Pong...').then((msg) => {
+   msg.edit (`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API latency is ${Math.round(client.ping)}ms.`);
+});
+}
+
+}
+
+module.exports.help = {
+  name: "ping"
+}
