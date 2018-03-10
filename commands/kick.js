07f3833f47e24062c01c2a7d510f2b3832a3e8ae
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-10 18:16:39 +0000

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,6 +2,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if(args[0] == "help"){
+        message.reply("Usage: G.kick <user>");
+        return;
+      }
     if("kUser") return message.channel.send("Can't find user!");
     let kReason = args.join(" ").slice(22);
     if(!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send("No can do pal!");
