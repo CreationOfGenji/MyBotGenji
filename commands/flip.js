f0e38c4ba9eac4ace1c05cbf6db65f30f88331cc
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-10 15:51:33 +0000

    Update flip.js

--- commands/flip.js
+++ commands/flip.js
@@ -10,7 +10,7 @@ let question = args.slice(0).join(" ");
 let flipembed = new Discord.RichEmbed()
 .setColor("#1b0ece")
 .setAuthor(message.author.tag)
-.addField("The coin landed on", replies[result]+result)
+.addField("The coin landed on", replies[result])
 
 message.channel.send(flipembed);
 
