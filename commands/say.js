c5cd11e9310ce5bce60ebe269ff40315fd9cfac1
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-10 18:21:57 +0000

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,6 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
       if(!message.member.hasPermission("CONNECT")) return;
+          if(args[0] == "help"){
+        message.reply("Usage: G.say <message>");
+        return;
+      }
       const sayMessage = args.join(" ");
       message.delete().catch();
       message.channel.send(sayMessage);
