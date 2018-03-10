f937516ebb50646a6c21a353cf1dd207c29b12c9
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-10 15:26:47 +0000

    Update flip.js

--- commands/flip.js
+++ commands/flip.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
 let replies = ["Heads", "Tails",];
 
-let result = (Math.random(1,2)* replies.length)
+let result = Math.random() * (2-1) + 1
 let question = args.slice(0).join(" ");
 
 let flipembed = new Discord.RichEmbed()
