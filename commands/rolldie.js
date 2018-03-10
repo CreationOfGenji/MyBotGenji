c9f585d9fa8b9b0944246d6ece5a35b832765ea3
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-10 18:21:27 +0000

    Update rolldie.js

--- commands/rolldie.js
+++ commands/rolldie.js
@@ -1,8 +1,11 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-
       
+          if(args[0] == "help"){
+        message.reply("Usage: G.roll");
+        return;
+      }
 
          var roll = Math.floor(Math.random() * 6) + 1;
         message.reply("The die rolled on a " + roll);
