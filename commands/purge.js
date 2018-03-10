79964bbdf6c444865febd0784542608258d1a26e
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-10 17:36:47 +0000

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -2,9 +2,9 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
+  message.delete().catch();
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
   if(!args[0]) return message.channel.send("no");
-  message.delete().catch();
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(5000));
 });
