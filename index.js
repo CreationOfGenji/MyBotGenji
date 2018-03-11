1376470293da0a6305b151859fb72b9a0247df9f
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-11 19:47:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
+let xp = require("./xp.json");
 
 fs.readdir("./commands/", (err, file) => {
+
   
   if(err) console.log(err);
-
   let jsfile = file.filter(f => f.split(".").pop() === "js")
   if(jsfile.length <= 0){
     console.log("Couldn't Find Commands.");
@@ -16,39 +17,59 @@ fs.readdir("./commands/", (err, file) => {
 
   jsfile.forEach((f, i) =>{
     let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`)
+    console.log(`${f} loaded!`);
     bot.commands.set(props.help.name, props);
   });
-
 });
-
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
   bot.user.setActivity("Overwatch", {type: "PLAYING"});
+
 });
 
 bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
-  
- let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-  if(!prefixes[message.guild.id]){
-    prefixes[message.guild.id] = {
-     prefixes: botconfig.prefix
+
+  let xpAdd = Math.floor(Math.random() * 7) + 8;
+  console.log(xpAdd);
+
+  if(!xp[message.author.id]){
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
     };
   }
+  
 
-  let prefix = prefixes[message.guild.id].prefixes;
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 300;
+  xp[message.author.id].xp = curxp + xpAdd;
+  if(nxtLvl <= xp[message.author.id].xp){
+    xp[message.author.id].level = curlvl + 1;
+    let lvlup = new Discord.RichEmbed()
+    .setTitle("Level Up!")
+    .setColor("#3d1466")
+    .addField("New Level", curlvl + 1);
+
+    message.channel.send(lvlup);
+
+  }
+  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+    if(err) console.log(err)
+  });
+ 
+  let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  let commandfile = bot.commands.get(cmd.slice(prefix.length))
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(message.content.indexOf(prefix) !== 0) return;
   if(commandfile) commandfile.run(bot,message,args);
-    
   
 });
 
-bot.login(process.env.BOT_TOKEN);
+bot.login(botconfig.token);
