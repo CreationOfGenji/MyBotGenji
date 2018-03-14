3c6ccaabd8c5545559981f2fee49c89e882afe3d
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-14 18:54:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
-let xp = require("./xp.json");
 
 fs.readdir("./commands/", (err, file) => {
 
@@ -30,36 +29,7 @@ bot.on("ready", async () => {
 
 bot.on("message", async message => {
   if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-
-  let xpAdd = Math.floor(Math.random() * 7) + 8;
-  console.log(xpAdd);
-
-  if(!xp[message.author.id]){
-    xp[message.author.id] = {
-      xp: 0,
-      level: 1
-    };
-  }
-  
-
-  let curxp = xp[message.author.id].xp;
-  let curlvl = xp[message.author.id].level;
-  let nxtLvl = xp[message.author.id].level * 300;
-  xp[message.author.id].xp = curxp + xpAdd;
-  if(nxtLvl <= xp[message.author.id].xp){
-    xp[message.author.id].level = curlvl + 1;
-    let lvlup = new Discord.RichEmbed()
-    .setTitle("Level Up!")
-    .setColor("#3d1466")
-    .addField("New Level", curlvl + 1);
-
-    message.channel.send(lvlup);
-
-  }
-  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
-    if(err) console.log(err)
-  });
+  if(message.channel.type === "dm") return              
  
   let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
