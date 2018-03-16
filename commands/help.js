db12402add5d533ad610b96b3a1c41048d3b2449
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-16 19:27:08 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,8 +8,8 @@ module.exports.run = async (bot, message, args) => {
     .addField("Member Commands:", "help, serverinfo, botinfo, userinfo")
     .addField("Fun Commands:", "roll, flip, say")
     .addField("Misc:", "ping, avatar, help, prefix")
-    .addField("Credits:", "Credits to Dovixx#1884 For Helping Me Fix Bugs!")
     .addField("Mod Commands", "addrole, removerole, kick, ban, purge, mute")
+    .addField("Credits:", "Credits to Dovixx#1884 and sebi's bot tutorial For Helping Me Fix Bugs!")
     .addField("Support Server:", "https://discord.gg/EEPHpuS");
 
     message.channel.send(helpembed);
