92a6b1f03701f18a467c4acf2e0a6ec6bc366e73
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-16 17:55:01 +0000

    Update userinf.js

--- commands/userinf.js
+++ commands/userinf.js
@@ -6,8 +6,8 @@ module.exports.run = async (bot, message, args) => {
 
     let userEmbed = new Discord.RichEmbed()
     .setDescription("Userinfo")
-    .setTitle(`${uUser.displayAvatarURL}`)
     .setColor("#2e26d3")
+    .setThumbnail(`${uUser.displayAvatarURL}`)
     .addField("Username:", `${uUser}`)
     .addField("User ID:", `${uUser.id}`)
     .addField("Joined On", message.guild.joinedAt)
