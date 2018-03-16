25ba9870a4b81ee2c16909f3f4f8d0b38ada14a0
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-16 17:50:15 +0000

    Update userinf.js

--- commands/userinf.js
+++ commands/userinf.js
@@ -2,13 +2,15 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let uUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!uUser) return message.channel.send("Coulden't Find User.");
+    if(!uUser) return message.channel.send("Couldn't Find User.");
 
     let userEmbed = new Discord.RichEmbed()
     .setDescription("Userinfo")
+    .setTitle(`${uUser.displayAvatarURL}`)
     .setColor("#2e26d3")
     .addField("Username:", `${uUser}`)
-    .addField("User ID:", `${uUser.id}`);
+    .addField("User ID:", `${uUser.id}`)
+    .addField("Joined On", message.guild.joinedAt)
 
     message.channel.send(userEmbed)
 
