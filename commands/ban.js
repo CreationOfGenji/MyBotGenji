2299e9d70abbb3aeee28666499a1020aa2f0fe8e
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-24 12:04:17 +0000

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -8,8 +8,8 @@ module.exports.run = async (bot, message, args) => {
       }
     if(!bUser) return message.channel.send("Can't find user!");
     let bReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_ROLES, MANAGE_SERVER")) return message.channel.send("No can do pal!");
-    if(bUser.hasPermission("MANAGE_ROLES, MANAGE_SERVER")) return message.channel.send("That person can't be banned!");
+    if(!message.member.hasPermission("MANAGE_ROLES", "MANAGE_SERVER")) return message.channel.send("No can do pal!");
+    if(bUser.hasPermission("MANAGE_ROLES", "MANAGE_SERVER")) return message.channel.send("That person can't be banned!");
 
     let banEmbed = new Discord.RichEmbed()
     .setDescription("~Ban~")
