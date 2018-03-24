78937a9d6d05b599a02df1a74c96793c7f3d54a9
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-24 09:38:04 +0000

    Delete madamada.js

--- commands/madamada.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    if(message.content.startsWith(botconfig.prefix + madamada))
-        message.channel.send("Mada Mada!");
-
-}
