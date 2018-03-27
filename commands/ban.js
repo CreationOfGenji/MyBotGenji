61d5c5094d7a5198e9d684573861085545d60945
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2018-03-27 07:10:41 +0000

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
+    if(!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send("No can do pal!");
     if(args[0] == "help"){
       message.reply("Usage: !ban <user> <reason>");
       return;
